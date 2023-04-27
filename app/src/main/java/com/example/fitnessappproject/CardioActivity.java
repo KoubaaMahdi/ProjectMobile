07230af4225f69af20f23575ffc1b61252d53852
KoubaaMahdi <tikou_a@live.fr>
2023-04-27 10:40:32 +0100

    Final Build Buz Sbeh

--- app/src/main/java/com/example/fitnessappproject/CardioActivity.java
+++ app/src/main/java/com/example/fitnessappproject/CardioActivity.java
@@ -7,24 +7,28 @@ import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+
+
 public class CardioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int i;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardio);
+        int i;
         VideoView videoView[] = {findViewById(R.id.video_view_jumping_jacks),findViewById(R.id.video_high_knees),findViewById(R.id.video_burpees)};
-
         Uri videoUri [] = {Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.exp1),Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.exp2),Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.exp3)};
-        // Set optional MediaController to enable controls (such as play/pause) for the video
-       for (i = 0 ; i< 3;i++){
-           MediaController mediaController = new MediaController(this);
-           mediaController.setAnchorView(videoView[i]);
-           videoView[i].setMediaController(mediaController);
-           videoView[i].setVideoURI(videoUri[i]);
+        MediaController[] mediaController=new MediaController[3];
 
-       }
+        // Set optional MediaController to enable controls (such as play/pause) for the video
+        for (i = 0 ; i< 3;i++){
+            mediaController[i]=new MediaController(this);
+            mediaController[i].setAnchorView(videoView[i]);
+            videoView[i].setMediaController(mediaController[i]);
+            videoView[i].setVideoURI(videoUri[i]);
+
+        }
 
     }
 }
